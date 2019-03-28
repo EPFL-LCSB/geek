5ba4b3745d095cac1572abfc03eb3c5e1ba7b94e
Daniel <daniel.weilandt@epfl.ch>
2019-03-28 10:36:14 +0100

    MTN: Modifiy the setup file

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setup(name='geek',
                         'scipy',
                         'pandas',
                         'statsmodels',
-                        'hsbrd',],
+                        'openbread',],
 
       packages = find_packages(),
       python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, <4',
