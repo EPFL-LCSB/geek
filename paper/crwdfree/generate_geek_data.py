FIX: Correct calculation of initial reates

paper/crwdfree/generate_geek_data.py
@@ -49,7 +49,7 @@ parameters_diff_lim = {
     'A_0': 50e-6,       # M
     'B_0': 50e-6,       # M
     'C_0': 50e-6,       # M
-    'volume': 1e-18,    # L
+    'volume': 10e-18,    # L
     't_max': 1e-6,      # s
     'dt': 1.e-9,        # s
     'mu_mass': 21.1,    # s
@@ -113,11 +113,11 @@ def run_simulation(parameters,phi,seed):
     n = round(0.5 * len(result.collisions))
     reaction_propablity = 1 - np.exp( -effective_k_binding*dt_log/volume_AB)
 
-    k_1_bwd_eff_rel = np.mean(result.acceptance[n:-1])/100.0
+    k_1_bwd_eff_rel = np.mean(result.acceptance[n:-1])/100.0/float(result.species['C'][0])
     k_1_bwd_eff = parameters['k_fwd'] * parameters['K_eq'] * k_1_bwd_eff_rel
 
-    k_1_fwd_eff = np.mean(result.collisions[n:-1]) / (dt_log * result.species['A'][0] * result.species['B'][0]) \
-                  * reaction_propablity
+    k_1_fwd_eff = np.mean(result.collisions[n:-1]) / (dt_log * parameters['A_0'] * parameters['B_0']) \
+                  * reaction_propablity /  (1e-18 * 1000 * AVOGADRO_NUMBER)
     k_1_fwd_eff_rel = k_1_fwd_eff / parameters['k_fwd']
 
     data = np.array([[parameters['A_0'],

paper/crwdfree/generate_job_files_validation_react_cluster.py
@@ -48,9 +48,9 @@ A_ref = 50e-6
 B_ref = 50e-6
 C_ref = 50e-6
 
-A_concentrations = np.array([0.25, 0.5, 1., 2.0, 4.0 ])*1.0
-B_concentrations = np.array([0.25, 0.5, 1., 2.0, 4.0 ])*1.0
-C_concentrations = np.array([0.25, 0.5, 1., 2.0, 4.0 ])*1.0
+A_concentrations = np.array([0.25, 0.5, 1., 2.0,])*1.0
+B_concentrations = np.array([0.25, 0.5, 1., 2.0,])*1.0
+C_concentrations = np.array([0.25, 0.5, 1., 2.0,])*1.0
 
 
 